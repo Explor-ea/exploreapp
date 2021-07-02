be767f9aa03931bb25c7bedc38f709d57d340c5a
cedrix <cedricbardaine@gmail.com>
2021-07-02 14:00:13 +0200

    shrink seconds wait time & add TODO

--- lib/pages/start_screens/splash_screen.dart
+++ lib/pages/start_screens/splash_screen.dart
@@ -3,13 +3,15 @@ import 'dart:async';
 import 'package:exploreapp/pages/start_screens/newcomer/home.dart';
 import 'package:flutter/material.dart';
 
+// TODO: display splash screen animation
+// TODO: change Duration seconds
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Timer(
-        Duration(seconds: 2),
+        Duration(seconds: 1),
         () => Navigator.of(context).pushReplacement(
             MaterialPageRoute(builder: (BuildContext context) => Home())));
 
