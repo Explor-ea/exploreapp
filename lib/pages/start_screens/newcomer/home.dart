9460c02548ce5d4ab301769cb6ba08ad843333e4
cedrix <cedricbardaine@gmail.com>
2021-07-02 14:24:54 +0200

    work a little on home

--- lib/pages/start_screens/newcomer/home.dart
+++ lib/pages/start_screens/newcomer/home.dart
@@ -7,28 +7,40 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          children: [
-            Text(
-              "Explor'ea",
-              style: TextStyle(color: Colors.black, fontSize: 16.0),
-            ),
-            Container(height: 32.0),
-            Text(
-              "Fugiat consectetur commodo pariatur cupidatat exercitation ullamco ut pariatur amet eiusmod consequat minim eiusmod. Consectetur aute magna enim irure ex et dolor enim. Excepteur consequat reprehenderit et cillum eiusmod consequat dolore aliqua esse ad sint et eiusmod.",
-              style: TextStyle(color: Colors.black, fontSize: 16.0),
-            ),
-            Container(height: 32.0),
-            ElevatedButton(
-                onPressed: () => Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => Home())),
-                child: Text("C'est parti !"))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Center(
+                  child: Text(
+                    "Explor'ea",
+                    style: TextStyle(color: Colors.black, fontSize: 32.0),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Text(
+                  "Fugiat consectetur commodo pariatur cupidatat exercitation ullamco ut pariatur amet eiusmod consequat minim eiusmod. Consectetur aute magna enim irure ex et dolor enim. Excepteur consequat reprehenderit et cillum eiusmod consequat dolore aliqua esse ad sint et eiusmod.",
+                  style: TextStyle(color: Colors.black, fontSize: 16.0),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: ElevatedButton(
+                      onPressed: () => Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => Home())),
+                      child: Text("C'est parti !")),
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
