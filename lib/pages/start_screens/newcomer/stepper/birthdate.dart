f37b5b3e613df26eaaa2a5e825fe64d4af058fce
cedrix <cedricbardaine@gmail.com>
2021-07-09 20:07:57 +0200

    WIP: add DatePicker

--- lib/pages/start_screens/newcomer/stepper/birthdate.dart
+++ lib/pages/start_screens/newcomer/stepper/birthdate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // TODO: was here : add date picker and save date, maybe with : https://flutter.dev/docs/cookbook/persistence/key-value
 class Birthdate extends StatelessWidget {
@@ -6,6 +7,9 @@ class Birthdate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FocusNode tf_focusNode1 = new FocusNode();
+    FocusNode tf_focusNode2 = new FocusNode();
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(32.0),
@@ -22,7 +26,52 @@ class Birthdate extends StatelessWidget {
             Expanded(
               flex: 5,
               child: Center(
-                child: Text("..."),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: "Jour"),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        maxLength: 2,
+                        onFieldSubmitted: (String textValue) => {
+                          FocusScope.of(context).requestFocus(tf_focusNode1)
+                        },
+                        validator: (textValue) {
+                          // TODO: valid two length caractere like : https://stackoverflow.com/a/61197837
+                        },
+                      ),
+                    ),
+                    Flexible(
+                      child: TextField(
+                        decoration: InputDecoration(labelText: "Mois"),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        maxLength: 2,
+                        focusNode: tf_focusNode1,
+                        onSubmitted: (String textValue) => {
+                          FocusScope.of(context).requestFocus(tf_focusNode2)
+                        },
+                      ),
+                    ),
+                    Flexible(
+                      child: TextField(
+                        decoration: InputDecoration(labelText: "Année"),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        maxLength: 4,
+                        focusNode: tf_focusNode2,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
