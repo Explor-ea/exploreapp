ac73c62d40b5fe88cbcd535b8b98fbf67f73d1de
cedrix <cedric.bardaine@kermap.com>
2021-09-11 13:09:39 +0200

    add FLeaflet & init map page

--- lib/pages/interactive_map.dart
+++ lib/pages/interactive_map.dart
@@ -1,10 +1,75 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class InteractiveMap extends StatelessWidget {
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class InteractiveMap extends StatefulWidget {
   const InteractiveMap({Key? key}) : super(key: key);
 
   @override
+  _InteractiveMapState createState() => _InteractiveMapState();
+}
+
+class _InteractiveMapState extends State<InteractiveMap> {
+  late final MapController mapController;
+  late final StreamSubscription<MapEvent> subscription;
+  //
+  // // Enable pinchZoom and doubleTapZoomBy by default
+
+  @override
+  void initState() {
+    super.initState();
+    mapController = MapController();
+
+    subscription = mapController.mapEventStream.listen(onMapEvent);
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+
+    super.dispose();
+  }
+
+  void onMapEvent(MapEvent mapEvent) {
+    if (mapEvent is! MapEventMove && mapEvent is! MapEventRotate) {
+      // do not flood console with move and rotate events
+      // print(mapEvent);
+    }
+  }
+
+  Widget buildMap() {
+    return FlutterMap(
+      mapController: mapController,
+      options: MapOptions(
+        center: LatLng(48.163, -2.73),
+        zoom: 7.0,
+        maxZoom: 18,
+        minZoom: 5,
+        rotation: 0.0,
+        nePanBoundary: LatLng(51.305, 8.233),
+        swPanBoundary: LatLng(42.33, -5.452),
+        slideOnBoundaries: true,
+        interactiveFlags: (InteractiveFlag.pinchZoom |
+            InteractiveFlag.doubleTapZoom |
+            InteractiveFlag.pinchMove |
+            InteractiveFlag.drag),
+      ),
+      layers: [
+        TileLayerOptions(
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c'],
+        ),
+      ],
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: buildMap(),
+    );
   }
 }
