6fa0c545435b43bf0ed8ba3efe65a6a42d8dad09
cedrix <cedric.bardaine@kermap.com>
2021-09-11 10:09:11 +0200

    prepare interactive map page

---
+++ lib/pages/interactive_map.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InteractiveMap extends StatelessWidget {
+  const InteractiveMap({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
