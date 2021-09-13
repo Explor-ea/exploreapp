ff2a0ee0ebae24de276c36d38e78d9bc0fa31f02
cedrix <cedric.bardaine@kermap.com>
2021-09-13 18:52:52 +0200

    pass static final Color_s to const

--- lib/explorea_colors.dart
+++ lib/explorea_colors.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/rendering.dart';
 
 class ExploreaColors {
-  static final yellow = Color(0xffF3B817); // #F3B817
-
-  static final purple = Color(0xff5A11B6); // #5A11B6
+  static const yellow = Color(0xffF3B817); // #F3B817
+  static const purple = Color(0xff5A11B6); // #5A11B6
 }
