a60101e63d315346ad886c75c33bc0c2d427f1b1
cedrix <cedricbardaine@gmail.com>
2021-09-14 13:54:07 +0200

    try to force to hide phone bars (notif and nav)

--- lib/pages/interactive_map.dart
+++ lib/pages/interactive_map.dart
@@ -3,7 +3,9 @@ import 'dart:async';
 import 'package:exploreapp/explorea_colors.dart';
 import 'package:exploreapp/pages/profile.dart';
 import 'package:exploreapp/wigets/explorea_fab.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -25,7 +27,9 @@ class _InteractiveMapState extends State<InteractiveMap> {
 
   @override
   void initState() {
+    if (!kIsWeb) SystemChrome.setEnabledSystemUIOverlays([]);
     super.initState();
+
     mapController = MapController();
 
     subscription = mapController.mapEventStream.listen(onMapEvent);
