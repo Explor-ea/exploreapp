457fa93c1a9f597a05166e24a7114bcea962fa8c
cedrix <cedricbardaine@gmail.com>
2021-09-15 08:35:28 +0200

    lock orientation to portrait

--- lib/pages/start_screens/newcomer.dart
+++ lib/pages/start_screens/newcomer.dart
@@ -2,6 +2,7 @@ import 'package:exploreapp/explorea_colors.dart';
 import 'package:exploreapp/wigets/explorea-text.dart';
 import 'package:exploreapp/wigets/explorea-title.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Newcomer extends StatefulWidget {
   const Newcomer({Key? key}) : super(key: key);
@@ -11,6 +12,21 @@ class Newcomer extends StatefulWidget {
 }
 
 class _NewcomerState extends State<Newcomer> {
+  @override
+  initState() {
+    super.initState();
+    // XXX MAYBE: make it responsive instead
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  }
+
+  @override
+  dispose() {
+    SystemChrome.setPreferredOrientations([]);
+
+    super.dispose();
+  }
+
   String text =
       "Tempor non non ullamco labore voluptate consectetur. Consectetur aliquip aliqua id sit incididunt proident deserunt eu. Magna veniam mollit quis ullamco ut cillum pariatur. Duis nulla consequat laboris proident laborum. Anim consectetur duis eu consectetur voluptate eiusmod. Esse aliquip veniam nisi anim consequat velit nulla. Est aute adipisicing eu veniam laboris enim aute irure. Aliqua sunt culpa esse fugiat sint qui qui officia et sunt laboris cillum nulla. Cupidatat est enim nostrud consequat dolor sunt qui magna pariatur. Elit cupidatat duis aute eiusmod velit pariatur aute nostrud minim elit in. Velit cupidatat minim quis incididunt in est deserunt consectetur cillum nisi ullamco velit anim. Minim dolor commodo labore dolor. Sit aliquip qui amet id occaecat irure commodo minim nisi ad sunt. Velit enim sint sint laborum dolore nisi cillum consectetur. Ad occaecat excepteur minim duis laborum ullamco et qui elit deserunt Lorem velit nisi. Ea anim laboris adipisicing minim nisi deserunt enim consequat. Quis est duis quis ea. Adipisicing enim amet ad sint consequat adipisicing magna. Deserunt dolor commodo non voluptate adipisicing magna amet magna ad minim consequat nostrud. Sunt commodo do sit occaecat. Laboris laboris dolore dolor labore velit duis pariatur laborum ullamco. Aute excepteur ut proident nisi commodo voluptate ad qui qui culpa. Id dolor officia excepteur sit dolore. ";
 
@@ -52,8 +68,8 @@ class _NewcomerState extends State<Newcomer> {
                       children: [
                         Expanded(
                           flex: 2,
-                          child: SingleChildScrollView(
-                              child: ExploreaText(text: this.text)),
+                            child: SingleChildScrollView(
+                                child: ExploreaText(text: this.text)),
                         ),
                       ],
                     ),
@@ -71,7 +87,7 @@ class _NewcomerState extends State<Newcomer> {
                       //                 borderRadius:
                       //                     BorderRadius.circular(100)))),
                       style: ElevatedButton.styleFrom(
-                          shape: StadiumBorder(),
+                        shape: StadiumBorder(),
                           primary: ExploreaColors.purple),
                       child: Text("C'est parti !"),
                     ),
