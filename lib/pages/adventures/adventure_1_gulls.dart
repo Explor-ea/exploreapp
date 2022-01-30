0f512a7b26b38c9bdd4e8724d20efcd5a1869ba9
cedrix <cedricbardaine@gmail.com>
2022-01-30 18:05:18 +0100

    can display a video

--- lib/pages/adventures/adventure_1_gulls.dart
+++ lib/pages/adventures/adventure_1_gulls.dart
@@ -1,5 +1,8 @@
+import 'package:exploreapp/explorea_colors.dart';
 import 'package:exploreapp/wigets/explorea_timer.dart';
 import 'package:flutter/material.dart';
+
+import 'package:video_player/video_player.dart';
 
 class Adventure1Gulls extends StatefulWidget {
   const Adventure1Gulls({Key? key}) : super(key: key);
@@ -9,17 +12,58 @@ class Adventure1Gulls extends StatefulWidget {
 }
 
 class _Adventure1GullsState extends State<Adventure1Gulls> {
+  late VideoPlayerController _vpController;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    this._vpController =
+        VideoPlayerController.asset("assets/adventure_1_gulls/SCREEN01.mp4");
+
+    this._vpController.setLooping(false);
+
+    this._vpController.initialize();
+
+    this._vpController.play();
+
+    // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            height: 30.0,
-          ),
           ExploreaTimer(
-            totalTime: Duration(seconds: 68),
+            totalTime: Duration(seconds: 60 * 15),
+            color: ExploreaColors.purple,
           ),
+          // Center(
+          //     child: FutureBuilder(
+          //   future: this._initializeVideoPlayerFuture,
+          //   builder: (context, snapshot) {
+          //     if (snapshot.connectionState == ConnectionState.done)
+          //       return AspectRatio(
+          //         aspectRatio: this._vpController.value.aspectRatio,
+          //         child: VideoPlayer(this._vpController),
+          //       );
+          //     else
+          //       return const Center(
+          //         child: CircularProgressIndicator(),
+          //       );
+          //   },
+          // ))
+
+          //
+
+          // _vpController!.value.isInitialized ?
+          AspectRatio(
+            aspectRatio: 9.0 / 16.0,
+            child: VideoPlayer(this._vpController),
+          )
         ],
       ),
     );
