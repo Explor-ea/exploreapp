e2bd51aa604075f210fdcc434b074de2efb26d31
cedrix <cedricbardaine@gmail.com>
2022-02-21 23:33:46 +0100

    add Explorea cinematic

--- lib/pages/start_screens/cinematic.dart
+++ lib/pages/start_screens/cinematic.dart
@@ -1,46 +1,36 @@
-// import 'dart:html';
-
-import 'dart:async';
-
+import 'package:exploreapp/explorea_colors.dart';
 import 'package:exploreapp/pages/interactive_map.dart';
 import 'package:exploreapp/src/navigation.dart';
 import 'package:flutter/material.dart';
-import 'package:exploreapp/main.dart';
-
-import '../../pass_points.dart';
+import 'package:video_player/video_player.dart';
 
 class Cinematic extends StatelessWidget {
   const Cinematic({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 1), () async {
-      goToNextPage(context, InteractiveMap());
+    VideoPlayerController _vpController =
+        new VideoPlayerController.asset("assets/EXPLOREA_LOGO_16-9.mp4");
+
+    _vpController.initialize().then((nothing) {
+      _vpController.play();
+
+      _vpController.addListener(() {
+        if (_vpController.value.position == _vpController.value.duration) {
+          goToNextPage(context, InteractiveMap());
+        }
+      });
     });
 
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(32.0),
-      child: Column(
-        children: [
-          Expanded(
-              flex: 1,
-              child: Row(
-                children: [
-                  // TODO: replace text by a cinematic. https://drive.google.com/file/d/1VBDIgEO98Qtz_wuiuJXfrDfC36nguQ8T/view?usp=sharing
-                  Text(
-                    "Ptite cinématic",
-                  ),
-                ],
-              )),
-          Expanded(
-            flex: 5,
-            child: Column(),
-          ),
-          SizedBox(height: 16.0),
-          PassPoints(nbPoints: 0, nextPage: InteractiveMap()),
-        ],
-      ),
-    ));
+      // backgroundColor: ExploreaColors.yellow,
+      backgroundColor: ExploreaColors.purple,
+      body: Align(
+          alignment: Alignment.center,
+          child: AspectRatio(
+            aspectRatio: 9.0 / 16.0,
+            child: VideoPlayer(_vpController),
+          )),
+    );
   }
 }
